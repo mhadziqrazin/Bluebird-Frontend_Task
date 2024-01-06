7b1bd189ce89474ad484a63bcf33211db7c5fb0e
mhadziqrazin <mhadziq0403@gmail.com>
2024-01-07 01:50:05 +0700

    feat: category, cartype, type, and data interface

---
+++ src/components/interface.ts
@@ -0,0 +1,24 @@
+export interface Category {
+  id: number
+  name: string
+  imageURL: string
+}
+
+export interface CarType {
+  vehicle: string
+  imageURL: string
+  price: string
+  description: string[]
+}
+
+export interface Type {
+  id: number
+  category_id: number
+  car_type: CarType[]
+}
+
+export interface Data {
+  category: Category[]
+  type: Type[]
+}
+
