3a0086abedf071ba598532d483748a2fed14cc06
mhadziqrazin <mhadziq0403@gmail.com>
2024-01-07 03:45:26 +0700

    style: smaller nav category

--- src/components/elements/NavCategory.tsx
+++ src/components/elements/NavCategory.tsx
@@ -10,14 +10,14 @@ export const NavCategory: React.FC<NavCategoryProps> = ({ data }) => {
       <div className='container mx-auto flex sm:justify-center'>
         <ul className='flex gap-2 sm:gap-6 justify-center p-4 items-center'>
           {data.category.map((item) => (
-            <li key={item.id} className='min-w-[135px] flex flex-col items-center rounded-xl bg-black/5 p-2 cursor-pointer hover:shadow-xl transition-shadow duration-200'>
+            <li key={item.id} className='min-w-[125px] flex flex-col items-center rounded-xl bg-black/5 p-2 cursor-pointer hover:shadow-xl transition-shadow duration-200'>
               <Image
-                width={50}
-                height={50}
+                width={40}
+                height={40}
                 src={item.imageURL.split(' ').join('')}
                 alt={item.name}
               />
-              <p className='text-white font-light'>
+              <p className='text-sm text-white font-light'>
                 {item.name}
               </p>
             </li>
